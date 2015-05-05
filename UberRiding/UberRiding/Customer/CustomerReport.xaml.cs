bce6c685eff46fb32cda4d2d2d546b10a2e38d3f
Vidal <letrungvi@outlook.com>
2015-05-05 17:13:09 +0700

    so small

---
+++ UberRiding/UberRiding/Customer/CustomerReport.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace UberRiding.Customer
+{
+    public partial class CustomerReport : PhoneApplicationPage
+    {
+        public CustomerReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
